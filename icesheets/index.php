ee1d08513ebb1b48d8b8a28ba37480429f19b9d8
Alan Muir <a.muir@ucl.ac.uk>
2023-12-03 14:18:59 +0000

    update

---
+++ icesheets/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>EOCIS Ice Sheets</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body class="">
+
+  
+<div class=" p-4 ">
+    <div class="row">  
+         <div class="col">
+         <a href="http://eocis.org"><image src="images/cropped-EOCIS-Logo-Final.png" alt="EOCIS logo" width="200px" ></a>
+         </div>
+         <div class="col">
+         <h3 class="text-center">Antarctic and Greenland<br> Ice Sheet Elevation and Mass Change Data</h3>
+
+         </div>
+    </div>
+
+</div>
+
+<div class="bg-dark p-1 text-light">
+<p class="m-1">Surface Elevation and Mass Balance Data products will be available from Dec 2023.</p>
+</div>
+
+  <div class=" bg-light">
+    <div class="container pt-4 pb-4">
+    <div class="row">  
+         <div class="col">
+        <h4>Products</h4>
+        <p>Ice sheet elevation and mass balance processed monthly.
+Quarterly distribution and user report. Annual system reports.
+Annual scientific reports.</p>
+         <h4>Surface elevation change</h4>
+         <p>
+This data product is suitable for investigating where surface 
+elevation change occurs and the rate at which it occurs over the 
+Greenland and Antarctic ice sheets. 
+The elevation change product can also be used to explore the processes that 
+drive changes to the ice sheets. Ice sheet surface elevation change is indicative 
+of mass change. </p>
+<h4>Mass balance</h4><p>
+This data product is suitable for understanding the current state and changes to the Greenland and Antarctic ice sheets. By measuring mass balance at 5 km2 resolution, it will be possible to investigate spatiotemporal variability, where mass changes occur and the rate at which they occur. This information is crucial for estimating the ice sheet contribution to sea level rise.
+</p>
+        </div>
+        <div class="col">
+            
+            <div class="text-center">
+            <image src="images/eocis_ais_sec_example.png" alt="Ice Sheet SEC" width="100%" >
+        </div>
+        </div>
+        </div>
+    </div>
+</div>
+  </div>
+
+<div class="container-fluid">
+    <image src="images/sealevel_ts.gif" alt="sea level contribution" width="100%" >
+        <p class="text-center">Citation: Trends in Antarctic Ice Sheet Elevation and Mass is published in Geophysical Research Letters 16 May 2019 <br>DOI: 10.1029/2019GL082182</p>
+</div>
+
+</body>
+</html>
+
---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>EOCIS Sea Ice</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body class="">
+
+
+<div class=" p-4 ">
+    <div class="row">  
+         <div class="col">
+         <a href="http://eocis.org"><image src="images/cropped-EOCIS-Logo-Final.png" alt="EOCIS logo" width="200px" ></a>
+         </div>
+         <div class="col">
+         <h3 class="text-center">Cryosphere Products</h3>
+
+         </div>
+    </div>
+
+</div>
+
+<div class="bg-dark p-1 text-light">
+<p class="m-1">Ice Sheet and Sea Ice products will be available from Autumn 2023.</p>
+</div>
+
+  <div class=" bg-light">
+    <div class="container pt-4 pb-4">
+    <div class="row">  
+         <div class="col">
+        <h4>EOCIS Products Processed at CPOM</h4>
+        <p>For ice sheet products please follow <a href="icesheets">this link</a></p>
+        <p>For sea ice products please follow <a href="seaice">this link</a></p>
+        </div>
+        <div class="col">
+            
+            <div class="text-center">
+            <image src="images/eocis_ais_sec_example.png" alt="Ice Sheet SEC" width="50%" >
+        </div>
+        <div class="text-center">
+            <image src="images/sit_example.png" alt="Sea Ice Thickness" width="50%" >
+        </div>
+        </div>
+        </div>
+    </div>
+</div>
+  </div>
+
+</body>
+</html>
+
