7818e9f5e23d0316d20536bf45c77887ff0c9f93
Alan Muir <a.muir@ucl.ac.uk>
2023-12-05 12:04:53 +0000

    update

--- seaice/volume_timeseries2.php
+++ seaice/volume_timeseries2.php
@@ -1,8 +1,6 @@
 <?php  
 $vol_ts_file =  "timeseries_data/$mission/$farea/timeseries_".$basin_number."_volume.csv";
-if ($show_volume == 0) {
-    $vol_ts_file =  "timeseries_data/$mission/$farea/timeseries_".$basin_number."_thickness.csv";
-}
+
 //console_log($vol_ts_file);
 ?>
 <script>
